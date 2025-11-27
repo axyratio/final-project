60386b65dfeefe82ac8ef2dba27a1bec5ee0c470
axio <kittiphong.u-.66@ubu.ac.th>
2025-11-27 13:28:13 +0700

    use error handler, edit logic

--- 3_final_project/app/routes/store_application_router.py
+++ 3_final_project/app/routes/store_application_router.py
@@ -5,6 +5,7 @@ from app.schemas.store_application import RequestStoreApplication, ResponseStore
 from app.services import store_application_service
 from app.core.authz import authenticate_token, authorize_role
 from app.core.citizen_verified import citizen_verified
+from app.utils.response_handler import error_response
 
 router = APIRouter(prefix="/store", tags=["Store"])
 
@@ -12,25 +13,26 @@ router = APIRouter(prefix="/store", tags=["Store"])
     "/application",
     response_model=ResponseStoreApplication
 )
-def requestStoreApplication(
+def request_store_application(
     data: RequestStoreApplication,
-    auth_current_user: str = Depends(authenticate_token()),  # ใช้เพื่อยืนยันตัวตนผู้ใช้
-    auth_role: str = Depends(authorize_role(["user"])),  # ใช้เพื่อยืนยันตัวตนผู้ใช้
+    auth_current_user=Depends(authenticate_token()),
+    auth_role=Depends(authorize_role(["user"])),
     db: Session = Depends(get_db)
-):  
+):
     try:
+        # ✅ ตรวจสอบข้อมูลบัตรประชาชน
         card_verified = citizen_verified(data.card_id, data.first_name, data.last_name, data.birth_date)
-        print(f'first name {data.first_name}')
-        requestStoreApplicationRes, error = store_application_service.request_store_application_service(db, auth_current_user, data, card_verified)
-        if error:
-            raise HTTPException(status_code=400, detail=error)
-        
-        return ResponseStoreApplication(
-            message="Request store application successfully"
-        )
+
+        # ✅ เรียก service (service จะ return JSONResponse อยู่แล้ว)
+        return store_application_service.store_application_service(db, auth_current_user, data, card_verified)
 
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        return error_response(
+            "เกิดข้อผิดพลาดขณะสมัครร้านค้า",
+            {"error": str(e)},
+            status_code=500
+        )
+
 
 # ไม่ต้องทำถ้าหากมีระบบเช็คบัญชีธนาคารแล้ว 
 # def approvedStoreApplication(
