88c2497ac9b582484acc3a6a9e326e64a6313cd1
axio <kittiphong.u-.66@ubu.ac.th>
2025-12-24 13:03:58 +0700

    edit little

--- 3_final_project/app/models/shipping_address.py
+++ 3_final_project/app/models/shipping_address.py
@@ -1,6 +1,6 @@
 # app/models/shipping_address.py
 import uuid
-from sqlalchemy import Column, ForeignKey, String, TEXT, DateTime
+from sqlalchemy import Boolean, Column, ForeignKey, String, TEXT, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from app.db.database import Base
 from app.utils.now_utc import now_utc
@@ -11,15 +11,19 @@ class ShippingAddress(Base):
 
     ship_addr_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.user_id"), nullable=False)
+
     full_name = Column(String, nullable=False)
     phone_number = Column(String, nullable=False)
     address_line = Column(String, nullable=False)
-    province = Column(String, nullable=False)
-    district = Column(String, nullable=False)
-    sub_district = Column(String, nullable=False)
-    postal_code = Column(String, nullable=False)
-    created_at = Column(DateTime(timezone=True), default=now_utc)
-    updated_at = Column(DateTime(timezone=True), default=now_utc, onupdate=now_utc)
+
+    sub_district = Column(String, nullable=True)
+    district = Column(String, nullable=True)
+    province = Column(String, nullable=True)
+    postal_code = Column(String, nullable=True)
+
+    is_default = Column(Boolean, nullable=False, default=False)
+
+    created_at = Column(DateTime, default=now_utc)  # <-- ไม่มีวงเล็บ!
 
     # ✅ ความสัมพันธ์กับ User
     user = relationship("User", back_populates="shipping_addresses")
